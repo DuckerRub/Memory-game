21b0199da05161fbfee55445b2fda61b9fb5f387
Your Name <kedson.42@gmail.com>
2026-04-03 18:23:18 +0200

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -30,12 +30,16 @@ async function getPokemons () {
   }
 
   return randomPokemons;
+
 }
 
 export function App() {
 
-  const [pokemons, setPokemons] = useState([])
   const [fetchingPokemons, setFetchingPokemons] = useState(true)
+  const [pokemons, setPokemons] = useState([])
+  const [selectedPokemons, setSelectedPokemons] = useState([])
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
 
   useEffect(() => {
     async function loadPokemons () {
@@ -48,15 +52,92 @@ export function App() {
 
     if (fetchingPokemons) return <h1>Loading</h1>
 
+    function handleScore () {
+      setScore(score => score + 1)
+      if (score+1 > bestScore) setBestScore(() => score+1);
+    }
+
+    function shuffle () {
+      const copy = [...pokemons]
+      for (let i = copy.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [copy[i], copy[j]] = [copy[j], copy[i]];
+      }
+      setPokemons(copy)
+    }
+
+    function handleClick (id) {
+      shuffle()
+      if (selectedPokemons.includes(id)) {
+        setScore(0);
+        setSelectedPokemons([])
+      } else {
+        setSelectedPokemons([...selectedPokemons, id])
+        handleScore()
+      }
+    }
+
     return (
     <div className='container'>
       <div className='header'>
-        <div className='instructions'>Instructions</div>
-        <div className='score'>Score</div>
+        <div className='instructions'>Get points by clicking on an image but don't click on any more than once!</div>
+        <div className='score'>Score: {score}</div>
+        <div className='score'>Best score: {bestScore}</div>
       </div>
-      {!fetchingPokemons && <Images pokemons={pokemons}/>}
+      {!fetchingPokemons && 
+        <Images pokemons={pokemons} handleClick={handleClick}/>
+        }
     </div>
     )
 }
 
 export default App
+
+
+// [
+//   {
+//     "id": 1021,
+//     "name": "raging-bolt",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1021.png"
+//   },
+//   {
+//     "id": 894,
+//     "name": "regieleki",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/894.png"
+//   },
+//   {
+//     "id": 10083,
+//     "name": "pikachu-phd",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10083.png"
+//   },
+//   {
+//     "id": 667,
+//     "name": "litleo",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/667.png"
+//   },
+//   {
+//     "id": 10120,
+//     "name": "zygarde-complete",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10120.png"
+//   },
+//   {
+//     "id": 10091,
+//     "name": "rattata-alola",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10091.png"
+//   },
+//   {
+//     "id": 10109,
+//     "name": "geodude-alola",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10109.png"
+//   },
+//   {
+//     "id": 10276,
+//     "name": "terapagos-terastal",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10276.png"
+//   },
+//   {
+//     "id": 10155,
+//     "name": "necrozma-dusk",
+//     "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10155.png"
+//   }
+// ]
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,22 +1,14 @@
-import { useState, useEffect } from "react";
+import { ImageCard } from "./ImageCard.jsx";
 
 
-export function Images({pokemons}) {
-
-    function handleClick (pokemonName){
-        console.log(pokemonName)
-    }
-    
+export function Images({pokemons, handleClick}) {    
     return (
-        <div className="images">
-            {pokemons.map((p) => {
+     <div className="images">
+              {pokemons.map((p) => {
                 return (
-                <div key={p.id} className="pokemonCard" onClick={() => handleClick(p.name)}>
-                    <img src={p.image} width="100px"></img>
-                    <span>{p.name}</span>
-                </div>
+                    <ImageCard p={p} handleClick={handleClick} key={p.id}/>
                 )
             })}
-        </div>
+      </div>
     )
 }
